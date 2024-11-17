6a3387ad2cdbf5d1ae917d0ec2023282e7056567
horatiu <horatiulazar2003@gmail.com>
2024-11-17 12:50:53 +0200

    Laborator 3

--- Models/Book.cs
+++ Models/Book.cs
@@ -23,5 +23,7 @@ namespace Lazar_Horatiu_Lab2.Models
         public int? AuthorID { get; set; }
 
         public Author? Author { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
