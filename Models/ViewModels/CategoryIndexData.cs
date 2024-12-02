ffa1865b2cdc4266e143d99724cafa3a081ef3cb
HoratiuLazar <horatiulazar2003@gmail.com>
2024-12-02 16:12:01 +0200

    Laborator4 aproape gata

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Lazar_Horatiu_Lab2.Models;
+
+namespace Lazar_Horatiu_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Lazar_Horatiu_Lab2.Data;
 using Lazar_Horatiu_Lab2.Models;
+using Lazar_Horatiu_Lab2.Models.ViewModels;
 
 namespace Lazar_Horatiu_Lab2.Pages.Categories
 {
@@ -21,9 +22,26 @@ namespace Lazar_Horatiu_Lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.Books)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.Books;
+            }
         }
+        
     }
 }
